05fe768bb76ab95dd9882efcc6d608661391f4b7
ParvatiPatel <paro.patel@rocketmail.com>
2016-02-03 23:17:01 -0800

    first commit

---
+++ assignment1/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET services page. */
+router.get('/service', function(req, res, next) {
+  res.render('service', { title: 'Service' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About US' });
+});
+
+/* GET social page. */
+router.get('/social', function(req, res, next) {
+  res.render('social', { title: 'Social' });
+});
+
+/* GET contact page. */
+router.get('/ContactUs', function(req, res, next) {
+  res.render('ContactUs', { title: 'Social' });
+});
+
+module.exports = router;
